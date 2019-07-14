67e79c170fb2df6fb299f25db6f3a9cd3575b9ca
pbullion <pbullion@gmail.com>
2019-07-13 19:57:41 -0500

    updated api route

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
     this.state.isLoading = true;
     axios({
       method: "get",
-      url: `http://18.237.192.82:3008/dailygames`
+      url: `http://172.31.15.118:3008/dailygames`
     }).then(function(response) {
       console.log("response", response.data);
       self.state.games = response.data;
