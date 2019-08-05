9f5329ad2b1bf78830c69d7df075d515f3f3e50e
pbullion <pbullion@gmail.com>
2019-08-05 14:04:35 -0500

    u

--- src/components/CombinationsPage/CombinationPage.js
+++ src/components/CombinationsPage/CombinationPage.js
@@ -95,8 +95,8 @@ class CombinationPage extends Component {
     // });
     self.state.bettingGamesCombinations = self.getCombinations([
       "NYY",
-      "PHI",
-      "MIN"
+      "CWS",
+      "TB"
     ]);
   }
 
