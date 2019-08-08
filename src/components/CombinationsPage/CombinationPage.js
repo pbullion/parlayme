02344afca936f52c31c4faa309a7e13d80e7cb52
pbullion <pbullion@gmail.com>
2019-08-08 14:41:26 -0500

    updates

--- src/components/CombinationsPage/CombinationPage.js
+++ src/components/CombinationsPage/CombinationPage.js
@@ -95,8 +95,8 @@ class CombinationPage extends Component {
     // });
     self.state.bettingGamesCombinations = self.getCombinations([
       "NYY",
-      "HOU",
-      "BOS"
+      "PHI",
+      "ATL"
     ]);
   }
 
@@ -113,7 +113,7 @@ class CombinationPage extends Component {
               {" "}
               <Col xs={12}>
                 <h1>Combinations To Bet</h1>
-                <h2>Updated August 8, 2019</h2>
+                <h2>Updated August 9, 2019</h2>
                 {this.state.bettingGamesCombinations
                   ? this.state.bettingGamesCombinations.map((bet, index) => {
                       return (
